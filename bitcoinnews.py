004b0072549ed594b636ba1fec4dbce29a0aea2f
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-03-14 20:55:38 +0100

    feat: completed coinmarketcap scraper

--- bitcoinnews.py
+++ bitcoinnews.py
@@ -1,1 +1,21 @@
-# start writing code here ❤️
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_news():
+    url = 'https://coinmarketcap.com/'
+    
+    response = requests.get(url)
+    response_text = response.text
+    
+    soup = BeautifulSoup(response_text, 'html.parser')
+    
+    file = open('articles.txt', 'w')
+    
+    file.write(str(soup))
+    
+    file.close()
+    
+    
+    
+get_news()
--- coinmarketcap.py
+++ coinmarketcap.py
@@ -1,1 +1,29 @@
-# start writing code here ❤️
+import requests
+from bs4 import BeautifulSoup
+
+def get_crypto_prices():
+    url = 'https://coinmarketcap.com'
+    response_text = requests.get(url).text
+    
+    soup = BeautifulSoup(response_text, 'html.parser')
+    
+    # get all the table rows
+    table_rows = soup.findAll('tr')
+    
+    # iterate through all the table rows and get the required data
+    for table_row in table_rows:
+        crypto_name = table_row.find('p', class_ = 'sc-e225a64a-0 ePTNty')
+        shortened_crypto_name = table_row.find('p', class_ = 'sc-e225a64a-0 dfeAJi coin-item-symbol')
+        crypto_price = table_row.find('div', class_ = 'sc-8bda0120-0 dskdZn')
+        
+        if crypto_name is None or shortened_crypto_name is None or crypto_price is None:
+            continue
+        else:
+            crypto_name = crypto_name.text
+            shortened_crypto_name = shortened_crypto_name.text
+            crypto_price = crypto_price.text
+        
+            print(f"Name: {crypto_name} ({shortened_crypto_name}) \nPrice: {crypto_price}\n")
+    
+
+get_crypto_prices()
